7f204ba73dc44c38a0bc2df0eeae4f4beba2692a
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2023-07-19 09:31:29 +0900

    サインインページに画面遷移出来るように修正

--- sns_app/src/app.controller.ts
+++ sns_app/src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Param, Get, Render, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Injectable } from '@nestjs/common';
+import * as crypto from 'crypto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  // サインイン
+  @Get('/signin')
+  @Render('signin')
+  signin() {
+    return {
+      title: 'ログイン',
+    }
   }
 }
