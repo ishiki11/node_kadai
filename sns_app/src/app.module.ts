03199252fb317824011e5664abe33a83a0081d1b
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2023-07-19 09:23:11 +0900

    signupコントローラ作成

--- sns_app/src/app.module.ts
+++ sns_app/src/app.module.ts
@@ -3,12 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { SignupController } from './signup/signup.controller';
 
 @Module({
   imports: [ServeStaticModule.forRoot({
     rootPath: join(__dirname, '..', 'public'),
   })],
-  controllers: [AppController],
+  controllers: [AppController, SignupController],
   providers: [AppService],
 })
 export class AppModule {}
