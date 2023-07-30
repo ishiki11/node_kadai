ad8c9e21ba7d6c99684220a8d4fc2c3cb34c81e1
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2023-07-30 20:58:09 +0900

    セッションの設定 7日間保持

--- sns_app/src/main.ts
+++ sns_app/src/main.ts
@@ -4,7 +4,10 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { ValidationPipe } from '@nestjs/common';
 
+// hbsファイル利用
 const hbs = require('hbs');
+// Session利用
+const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -12,6 +15,13 @@ async function bootstrap() {
   hbs.registerPartials(join(__dirname, '..', 'views', 'partials'));
   app.setViewEngine('hbs');
   app.useGlobalPipes(new ValidationPipe());
+  app.use(
+    cookieSession({
+      name: 'session', // セッションクッキー名
+      keys: ['key1'], // クッキーの暗号化
+      maxAge: 7 * 24 * 60 * 60 * 1000, // セッションの有効期限
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
