cf5b26d2fe9b8551ef2c71d6efd7000c07b3ea67
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2023-07-30 21:24:25 +0900

    keyをランダムに変更

--- sns_app/src/main.ts
+++ sns_app/src/main.ts
@@ -3,11 +3,14 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { ValidationPipe } from '@nestjs/common';
+import * as crypto from 'crypto';
 
 // hbsファイル利用
 const hbs = require('hbs');
 // Session利用
 const cookieSession = require('cookie-session');
+// ランダムなkey生成
+const keys = crypto.randomBytes(16).toString('hex');
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -18,7 +21,7 @@ async function bootstrap() {
   app.use(
     cookieSession({
       name: 'session', // セッションクッキー名
-      keys: ['key1'], // クッキーの暗号化
+      keys: [keys], // クッキーの暗号化
       maxAge: 7 * 24 * 60 * 60 * 1000, // セッションの有効期限
     }),
   );
