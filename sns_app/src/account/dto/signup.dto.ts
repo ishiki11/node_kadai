09e57a2096bfe315ad9c0f1133d2f480d8ee08a0
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2023-07-30 15:33:35 +0900

    エラーメッセージ変更

--- sns_app/src/account/dto/signup.dto.ts
+++ sns_app/src/account/dto/signup.dto.ts
@@ -7,7 +7,7 @@ export class CreateAccountDto {
   @IsString()
   @Length(8, 20, { message: '8文字以上20文字以下で入力して下さい' })
   @Matches(/^(?=.*[a-zA-Z])(?=.*\d).{8,20}$/, {
-    message: '英字と数字を1字以上含む形で入力して下さい',
+    message: '英字と数字を含む形で入力して下さい',
   })
   password: string;
 }
