bb7e7928dcfec128c25118cd0d35add7dd86211f
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2023-07-30 11:06:38 +0900

    設定の変更

--- sns_app/src/app.module.ts
+++ sns_app/src/app.module.ts
@@ -4,14 +4,9 @@ import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { SignupController } from './Controller/signup/signup.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './entities/user.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
-    TypeOrmModule.forFeature([User]),
-    // UsersModule,
     ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'public') }),
   ],
   controllers: [AppController, SignupController],
