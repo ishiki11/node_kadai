343415f38256d657b307e52c8dc277de15696d6d
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2023-07-30 18:03:28 +0900

    post処理記述

--- sns_app/src/account/signin/signin.controller.ts
+++ sns_app/src/account/signin/signin.controller.ts
@@ -14,5 +14,37 @@ export class SigninController {
 
   // サインインの処理
   @Post()
-  async signinAccount(@Body() data: any, @Res() response) {}
+  async signinAccount(@Body() data: any, @Res() response) {
+    console.log(data);
+    // validation実行
+    const createAccountDto = new CreateAccountDto();
+    createAccountDto.email = data.email;
+    createAccountDto.password = data.password;
+    const errors = await validate(createAccountDto, {
+      validationError: { target: false },
+    });
+    if (errors.length > 0) {
+      // バリデーションエラーがある場合の処理
+      console.log(errors);
+      const flatErrors = errors.flatMap((error) =>
+        Object.values(error.constraints),
+      );
+      console.log('flat', flatErrors);
+      // errorをhbsに表示
+      return response.render('signup', {
+        errors: flatErrors,
+      });
+    } else {
+      try {
+        // validation通ったdata
+        // ログイン処理
+        return response.redirect('/');
+      } catch (error) {
+        console.log(error.message); // エラーが発生した場合はエラーメッセージを表示
+        return response.render('signup', {
+          errors: [error.message],
+        });
+      }
+    }
+  }
 }
