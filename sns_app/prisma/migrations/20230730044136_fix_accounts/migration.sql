354162b94ac4e03c7a81ee4aeced1b8aaea23da2
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2023-07-30 13:42:17 +0900

    saltの消去

---
+++ sns_app/prisma/migrations/20230730044136_fix_accounts/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `salt` on the `Accounts` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Accounts" DROP COLUMN "salt";
