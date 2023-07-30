7b66a8653cf84f59dd464d782b8b6e10fb817e4e
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2023-07-30 17:56:44 +0900

    getの処理記述

--- sns_app/src/account/signin/signin.controller.ts
+++ sns_app/src/account/signin/signin.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Render, Res } from '@nestjs/common';
+import { AccountService } from './../account.service';
+import { validate } from 'class-validator';
 
 @Controller('signin')
-export class SigninController {}
+export class SigninController {
+  constructor(private readonly accountService: AccountService) {}
+
+  // サインイン
+  @Get()
+  @Render('signin')
+  signUp(): void {}
+
+  // サインインの処理
+  @Post()
+  async signinAccount(@Body() data: any, @Res() response) {}
+}
